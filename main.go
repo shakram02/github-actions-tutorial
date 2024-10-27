b0b240a1644b44a9fa400f142cb273119ecddd6c
shakram02 <ahmedhamdyau@gmail.com>
2024-10-27 13:53:31 +0300

    feat: disable query param

--- main.go
+++ main.go
@@ -14,18 +14,16 @@ func (g *Greeter) Greet(name string) string {
 func handler(w http.ResponseWriter, r *http.Request) {
 	var greeter = Greeter{}
 
-	name := r.FormValue("name")
-	if name == "" {
-		name = "world"
-	}
-	fmt.Fprintf(w, greeter.Greet(name))
+	// name := r.FormValue("name")
+	// if name == "" {
+	// 	name = "world"
+	// }
+	// fmt.Fprintf(w, greeter.Greet(name))
 
-	// fmt.Fprintf(w, greeter.Greet("world"))
+	fmt.Fprintf(w, greeter.Greet("world"))
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":9010", nil)
 }
-
-// TODO: setup SSH
